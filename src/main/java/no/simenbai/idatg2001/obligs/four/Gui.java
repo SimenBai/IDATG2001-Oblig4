8129ada8839071c4790e9aa99252d8910da58a0f
Simen Bai <simen@4bai.no>
2020-03-18 22:53:50 +0100

    Added show details button

--- src/main/java/no/simenbai/idatg2001/obligs/four/Gui.java
+++ src/main/java/no/simenbai/idatg2001/obligs/four/Gui.java
@@ -16,7 +16,11 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.time.LocalDate;
 
 public class Gui extends Application {
     private Stage stage;
@@ -87,13 +91,7 @@ public class Gui extends Application {
 
         pointsColumn.setCellValueFactory(new PropertyValueFactory<>("points"));
         memberTypeColumn.setCellValueFactory(cellvalue -> {
-            if (cellvalue.getValue() instanceof BasicMember) {
-                return new SimpleStringProperty("Basic Member");
-            } else if (cellvalue.getValue() instanceof SilverMember) {
-                return new SimpleStringProperty("Silver Member");
-            } else {
-                return new SimpleStringProperty("Gold Member");
-            }
+            return new SimpleStringProperty(BonusMember.getMemberType(cellvalue.getValue()).toString());
         });
 
 
@@ -122,10 +120,9 @@ public class Gui extends Application {
     public GridPane getSettingMenu(TableView<BonusMember> tableView) {
         TableView.TableViewSelectionModel<BonusMember> selectionModel = tableView.getSelectionModel();
         selectionModel.setSelectionMode(SelectionMode.SINGLE);
-        
+
         Button detailsButton = new Button("See details of selected member");
         detailsButton.setOnAction(actionEvent -> {
-            System.out.println("Details");
             showDetails(selectionModel.getSelectedItem());
         });
 
@@ -136,7 +133,7 @@ public class Gui extends Application {
 
         Button upgradeButton = new Button("Upgrade eligible members");
         upgradeButton.setOnAction(actionEvent -> {
-            System.out.println("UPGRADE ");
+            memberArchive.checkMembers(LocalDate.now());
         });
 
         Button addButton = new Button("Add a members");
@@ -149,15 +146,77 @@ public class Gui extends Application {
         gSettings.setHgap(10);
         gSettings.setVgap(10);
         gSettings.setPadding(new Insets(20));
-        gSettings.add(addButton, 1,0);
+        gSettings.add(addButton, 1, 0);
         gSettings.add(detailsButton, 2, 0);
         gSettings.add(upgradeButton, 3, 0);
         gSettings.add(deleteButton, 4, 0);
         return gSettings;
     }
 
-    private void showDetails(BonusMember selectedItem) {
+    private void showDetails(BonusMember bonusMember) {
+        if(bonusMember == null){
+            return;
+        }
+        Stage dialog = new Stage();
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
 
+        Text displaying = new Text("Displaying member: ");
+        Text memberNo = new Text(Integer.toString(bonusMember.getMemberNo()));
+
+        Text fullNameLabel = new Text("Fullname: ");
+        Text fullName = new Text(
+                bonusMember.getPersonals().getFirstname() + " " +
+                        bonusMember.getPersonals().getSurname());
+
+        Text emailLabel = new Text("E-mail address: ");
+        Text email = new Text(bonusMember.getPersonals().getEMailAddress());
+
+        Text memberTypeLabel = new Text("Member type: ");
+        Text memberType = new Text(BonusMember.getMemberType(bonusMember).toString());
+
+        Text enrolledLabel = new Text("Enrolled date: ");
+        Text enrolled = new Text(bonusMember.getEnrolledDate().toString());
+
+        Text qPointslabel = new Text("Qualifying points: ");
+        Text qPoints = new Text(Integer.toString(bonusMember.findQualificationPoints(LocalDate.now())));
+
+        Text pointsLabel = new Text("Points: ");
+        Text points = new Text(Integer.toString(bonusMember.getPoints()));
+
+        Text spacer = new Text(" ");
+        Text spacer1 = new Text(" ");
+
+        gridPane.add(displaying, 0, 0);
+        gridPane.add(memberNo, 1, 0);
+
+        gridPane.add(fullNameLabel, 0, 1);
+        gridPane.add(fullName, 1, 1);
+
+        gridPane.add(emailLabel, 0, 2);
+        gridPane.add(email, 1, 2);
+
+        gridPane.add(spacer, 0, 3);
+
+        gridPane.add(memberTypeLabel, 0, 4);
+        gridPane.add(memberType, 1, 4);
+
+        gridPane.add(enrolledLabel, 0, 5);
+        gridPane.add(enrolled, 1, 5);
+
+        gridPane.add(qPointslabel, 0, 6);
+        gridPane.add(qPoints, 1, 6);
+
+        gridPane.add(pointsLabel, 0, 7);
+        gridPane.add(points, 1, 7);
+
+
+
+        gridPane.setPadding(new Insets(20));
+        dialog.setScene(new Scene(gridPane));
+        dialog.initOwner(this.stage);
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.showAndWait();
     }
 
     private ObservableList<BonusMember> getMemberList() {
